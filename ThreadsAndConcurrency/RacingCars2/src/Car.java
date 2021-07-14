ea0fea957bd772e22c760c1bc359a9b8c4620412
Harshil Shah <hshah3@neiu.edu>
2021-07-13 21:06:18 -0500

    assignment 4 Thread and Collection for cs420

--- ThreadsAndConcurrency/RacingCars2/src/Car.java
+++ ThreadsAndConcurrency/RacingCars2/src/Car.java
@@ -4,6 +4,25 @@ public class Car {
     public String name;
     public double maxMiles;
 
+    public Car(String name, double miles) {
+        this.name = name;
+        this.miles = miles;
+        odometer = 0;
+        this.maxMiles = miles;
+    }
+
+    public void move(double miles) {
+        this.miles = miles>0?this.miles -miles:0;
+        this.odometer+= miles;
+    }
+
+    public void charge() {
+        this.miles = maxMiles;
+    }
+
+    public String toString() {
+        return name + ": odo : " + odometer + "- miles left: " + miles;
+    }
  /* TODO 
 Exact same methods and constructor as the Car in task 1*/
 }
--- ThreadsAndConcurrency/RacingCars2/src/Racer.java
+++ ThreadsAndConcurrency/RacingCars2/src/Racer.java
@@ -1,3 +1,24 @@
-public class Racer /* TODO 
-This class is exactly as the Racer from Task 1*/
-}
+public class Racer implements Runnable {
+    Car c;
+    int distance = 1000;
+
+    public Racer(Car c){
+        this.c = c;
+    }
+    @Override
+    public void run() {
+        while(c.odometer<distance) {
+          if(c.miles == 0) {
+              System.out.println(c + "...Charging");
+              c.charge();
+              try{ Thread.sleep((int) c.miles);
+
+                }catch (InterruptedException e) {
+                  e.printStackTrace();
+                    }
+                } else {
+                  c.move(1);
+              }System.out.println("Finish: " + c);
+          }
+        }
+    }
